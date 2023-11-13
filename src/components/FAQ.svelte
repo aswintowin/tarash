a1070974621a02c61929049ff632ddb71701cafc
ashwin subramaniam <ashwin.subrmaniam@motorolasolutions.com>
2023-11-13 15:04:36 -0500

    update faqs

--- src/components/FAQ.svelte
+++ src/components/FAQ.svelte
@@ -17,12 +17,12 @@
 			{
 				header: 'How can I schedule a pick-up service?',
 				description:
-					"To arrange for pick-ups, you have two convenient options. You can either call us at 781-219-3469 or use our online ordering page to submit your request. If you'd like a same-day pick-up, please make sure to place your order by 11:00 AM, either online or over the phone. You can also leave your garments out for pick-up as late as 6:00 PM."
+					"To arrange for pick-ups, you have two convenient options. You can either call us at 781-660-4972 or use our online ordering page to submit your request. If you'd like a same-day pick-up, please make sure to place your order by 11:00 AM, either online or over the phone. You can also leave your garments out for pick-up as late as 6:00 PM."
 			},
 			{
 				header: 'What are your operating hours?',
 				description:
-					"To arrange for pick-ups, you have two convenient options. You can either call us at 781-219-3469 or use our online ordering page to submit your request. If you'd like a same-day pick-up, please make sure to place your order by 11:00 AM, either online or over the phone. You can also leave your garments out for pick-up as late as 6:00 PM."
+					"To arrange for pick-ups, you have two convenient options. You can either call us at 781-660-4972 or use our online ordering page to submit your request. If you'd like a same-day pick-up, please make sure to place your order by 11:00 AM, either online or over the phone. You can also leave your garments out for pick-up as late as 6:00 PM."
 			},
 			{
 				header: 'How are my clothing items tracked and managed?',
@@ -38,6 +38,16 @@
 				header: 'Delivery Minimum',
 				description:
 					' To ensure the seamless delivery of your laundry and dry cleaning by The Landing Laundromat, there is a minimum order requirement of 20 lbs or a $50.00 minimum charge for each delivery order.'
+			},
+			{
+				header: 'What is the last wash time?',
+				description:
+					'The posted time for starting the last wash is 9:00 PM. This typically allows enough time for our customers to finish washing, drying and folding their clothes by 10:00 PM.'
+			},
+			{
+				header: 'What is the last wash time?',
+				description:
+					'The posted time for starting the last wash is 9:00 PM. This typically allows enough time for our customers to finish washing, drying and folding their clothes by 10:00 PM.'
 			}
 		])
 	);
@@ -54,7 +64,7 @@
 	async function doPost() {
 		let body = {
 			first_name: name,
-			last_name: "",
+			last_name: '',
 			email,
 			phone,
 			comment: message
@@ -116,9 +126,11 @@
 					<Textarea {...textareaprops} bind:value={message} />
 				</div>
 				<div class="flex items-center mb-10">
-          <Button type="submit" on:click={doPost}>Submit</Button>
-          <p class="text-green-400 font-semibold text-lg px-4" hidden={!result}>Thank you! We will get in touch with you as soon as possible.</p>
-        </div>
+					<Button type="submit" on:click={doPost}>Submit</Button>
+					<p class="text-green-400 font-semibold text-lg px-4" hidden={!result}>
+						Thank you! We will get in touch with you as soon as possible.
+					</p>
+				</div>
 			</form>
 			<P weight="semibold" class="text-white m-4">Or call us</P>
 			<P weight="bold" class="text-white text-lg">(781) 660-4972</P>
--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,42 +1,42 @@
 <script>
-  import logo from "$lib/images/logo.webp";
-  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button, Input } from "flowbite-svelte";
-  let nonActiveClass = "text-lg";
+	import logo from '$lib/images/logo.webp';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button, Input } from 'flowbite-svelte';
+	let nonActiveClass = 'text-lg';
 </script>
 
 <Navbar class="dark:bg-white">
-  <NavBrand href="/">
-    <img src={logo} class="h-12 md:h-16" alt="The Landing Laundromat" />
-  </NavBrand>
-  <div class="flex md:order-2">
-    <phone>
-      <Button size="sm">Request free pickup</Button>
-    </phone>
-    <NavHamburger />
-  </div>
-  <NavUl {nonActiveClass} class="order-1">
-    <NavLi href="/">Home</NavLi>
-    <NavLi href="#faq">FAQ</NavLi>
-    <NavLi href="#services">Services</NavLi>
-    <NavLi href="#ameneties">Ameneties</NavLi>
-    <NavLi href="#contact">Contact</NavLi>
-  </NavUl>
+	<NavBrand href="/">
+		<img src={logo} class="h-12 md:h-16" alt="The Landing Laundromat" />
+	</NavBrand>
+	<div class="flex md:order-2">
+		<phone>
+			<Button size="sm">Request free pickup</Button>
+		</phone>
+		<NavHamburger />
+	</div>
+	<NavUl {nonActiveClass} class="order-1">
+		<NavLi href="/">Home</NavLi>
+		<NavLi href="#contact">Contact</NavLi>
+		<NavLi href="#services">Services</NavLi>
+		<NavLi href="#faq">FAQ</NavLi>
+		<NavLi href="#ameneties">Ameneties</NavLi>
+	</NavUl>
 </Navbar>
 
 <style>
-  phone {
-    animation: zoom-in-zoom-out 5s ease infinite;
-  }
+	phone {
+		animation: zoom-in-zoom-out 5s ease infinite;
+	}
 
-  @keyframes zoom-in-zoom-out {
-    0% {
-      transform: scale(1, 1);
-    }
-    50% {
-      transform: scale(1.1, 1.1);
-    }
-    100% {
-      transform: scale(1, 1);
-    }
-  }
+	@keyframes zoom-in-zoom-out {
+		0% {
+			transform: scale(1, 1);
+		}
+		50% {
+			transform: scale(1.1, 1.1);
+		}
+		100% {
+			transform: scale(1, 1);
+		}
+	}
 </style>
